dd3ba82aef2aa8b9a43d95e6a560f4163988e118
Andrew Dixon <andrew.dixon@principleone.co.uk>
2022-10-17 12:24:56 +0100

    initial commit for local/module file loading

--- myprojectname/main.py
+++ myprojectname/main.py
@@ -1,5 +1,20 @@
 def main():
     print("hello world")
 
+    load_module_file('filename')
+    load_runtime_file('filename')
+
+def load_module_file(filename: str):
+    """What goes here?"""
+    pass
+
+def load_runtime_file(filename: str):
+    """What goes here?"""
+    pass
+
+def read_file(filepath: str):
+    """I know how to do this generally"""
+    pass
+
 if __name__ == "__main__":
     exit(main())
